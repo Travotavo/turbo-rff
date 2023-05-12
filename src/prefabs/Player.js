91843fd5467ce0e369e4bd1c6d84d0ca0b0f4d78
Travis A. Carlen <tcarlen@ucsc.edu>
2023-05-11 17:13:37 -0700

    State machine implemented

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -1,42 +1,31 @@
 class Player extends Phaser.GameObjects.Sprite {
-    static States = {
-        Idle: 0,
-        Accelerating: 1
-    }
 
     constructor(scene, texture, frame, anim){
         super(scene, game.config.width/2, game.config.height/4 * 3, frame);
 
         scene.add.existing(this);
 
-        //Weak state machine needed
-        this.state = Player.States.Idle;
-
+        this.state = new IdleState(this);
 
         this.scene = scene;
         this.fuel = 1;
         this.play({key: anim, repeat: -1 });
     }
 
+    //Time to implement a state machine! Woohoo!
     update(delta){
-        if (Phaser.Input.Keyboard.JustDown(keyPress) && this.fuelCheck()){
-            this.state = Player.States.Accelerating;
-            this.play({key: 'Speed', repeat: -1});
-        }
-        if (Phaser.Input.Keyboard.JustUp(keyPress)){
-            this.state = Player.States.Idle;
-            this.play({key: 'Idle', repeat: -1});
-        }
+        this.handleInput();
+        this.state.update(delta);
 
-
-        if (this.state == Player.States.Accelerating){
-            this.fuel -= .1/delta;
-        }
-        else {
-            this.fuel += .01/delta;
-        }
         this.fuel = Math.min(Math.max(this.fuel, 0), 1);
         this.scene.fuelRefresh();
+    }
+
+    handleInput(){
+        let nextState = this.state.handleInput();
+        if (nextState != null){
+            this.state = nextState;
+        }
     }
 
     fuelCheck(){
@@ -44,5 +33,50 @@ class Player extends Phaser.GameObjects.Sprite {
             return true;
         }
         return false;
+    }
+}
+
+class PlayerState {
+    constructor(player){
+        this.player = player;
+    }
+    handleInput(){}
+    update(delta){}
+}
+
+class IdleState extends PlayerState{
+    constructor(player){
+        super(player);
+    }
+
+    handleInput(){
+        if (Phaser.Input.Keyboard.JustDown(keyPress) && this.player.fuelCheck()){
+            this.player.play({key: 'Speed', repeat: -1});
+            return new AcceleratingState(this.player);
+        }
+        return null;
+    }
+
+    update(delta){
+        this.player.fuel += .01/delta;
+    }
+}
+
+class AcceleratingState extends PlayerState{
+    constructor(player){
+        super(player);
+    }
+
+    handleInput(){
+        if (Phaser.Input.Keyboard.JustUp(keyPress)){
+            this.player.play({key: 'Idle', repeat: -1});
+            return new IdleState(this.player);
+        }
+        return null;
+    }
+
+    update(delta){
+        console.log('Accelerating');
+        this.player.fuel -= .1/delta;
     }
 }
